5be0d341ed4ef5a84d1493890d2568d795684f02
Henrique Souza <ricksantos.dev@gmail.com>
2026-05-31 17:31:17 -0300

    Create User model

---
+++ src/main/java/com/ricksantos/todosimple/model/User.java
@@ -0,0 +1,79 @@
+package com.ricksantos.todosimple.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "users")
+public class User {
+	
+	public interface CreateUser {}
+	public interface UpdateUser {}
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "username", length = 100, nullable = false, unique = true)
+	@NotNull(groups = CreateUser.class)
+	@NotEmpty(groups = CreateUser.class)
+	@Size(groups = CreateUser.class, min = 2, max = 100)
+	private String username;
+	
+	@JsonProperty(access = Access.WRITE_ONLY)
+	@Column(name = "password", length = 60, nullable = false)
+	@NotNull(groups = {CreateUser.class, UpdateUser.class})
+	@NotEmpty(groups = {CreateUser.class, UpdateUser.class})
+	@Size(groups = {CreateUser.class, UpdateUser.class}, min = 8, max = 60)
+	private String password;
+
+	public User() {
+	}
+
+    public User(Long id, String username, String password) {
+		this.id = id;
+		this.username = username;
+		this.password = password;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+    
+	
+	//private List<Task> tasks = new ArrayList<Task>();
+	
+}
